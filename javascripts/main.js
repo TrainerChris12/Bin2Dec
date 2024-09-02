c04885ab25cf56f4d9e3b71199c42e3a4f4116e9
TrainerChris12 <grantchristopher109@gmail.com>
2024-09-02 15:56:23 -0400

    Project finished with all requirements met. Could be better, but have satisfied the problem.

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,27 +6,51 @@
         the sequence must be calculated using a single mathematical function,
         for example the natural logarithm. It's up to you to figure out which function to use. */
 
-const digitInputs = document.querySelectorAll('form input');
+let digitInputs = document.querySelectorAll('#digitForm input');
 const digitForm = document.getElementById('digitForm');
 const convertBtn = document.querySelector('.convertBtn');
-const numberTypeMenu = document.getElementById('numberType');
+const numberLengthInput = document.getElementById('numberLength');
 
 // function that can handle base conversion for a given base
 function convertNumberBaseOf(base) {
 
     // for each input from the user calculate the value in base 10,
     // and add it to the previous value found
-        let acc = 0;
-        for (let i = 0; i < digitInputs.length; i++) {
-            let exponent =  (digitInputs.length - 1) - i;
-            let current = Number(digitInputs[i].value) * (base ** exponent);
-            acc += current;
-            console.log(acc);
-        }
+    let acc = 0;
+    for (let i = 0; i < digitInputs.length; i++) {
+        let exponent =  (digitInputs.length - 1) - i;
+        let current = Number(digitInputs[i].value) * (base ** exponent);
+        acc += current;
+    }
+    return acc;
 }
 
 
+// event listener that creates n amount of input fields
+numberLengthInput.addEventListener('input', e => {
+    //reset each time to ensure the amount of input fields does NOT accumulate with each input change
+    digitForm.innerHTML = ` <input 
+        class="digitFormInput" 
+        type="number" 
+        min="0" 
+        max="1" 
+        maxlength="1" 
+        required 
+        name="digit1"> `
 
+    // loop that creates a new digit input field n times, where n = numberLength
+    let numberLength = e.target.value;
+    for (let i = 0; i < numberLength; i++) {
+        if (numberLengthInput.value === '') continue;
+        if (numberLengthInput.value === '') continue;
+        const inputElementClone = digitInputs[0].cloneNode(true);
+        digitForm.appendChild(inputElementClone);
+    }
+    digitInputs = document.querySelectorAll('#digitForm input');
+})
+
+
+/*              Validate Input              */
 digitForm.addEventListener('input', e => {
     const input = e.target;
 
@@ -34,6 +58,7 @@ digitForm.addEventListener('input', e => {
     if (input.value.length > 1) {
         input.value = input.value.slice(0, 1);
     }
+
     // ensures that the input is either '1' or '0'
         if (input.value !== '1' && input.value !== '0') {
             input.value = '';
@@ -47,20 +72,20 @@ digitForm.addEventListener('input', e => {
         }
 });
 
-// I don't think this is the right approach, I want to check when the select menu is a certain option
-// and apply the correct parameter to the function accordingly.
-numberTypeMenu.addEventListener('change', (e) => {
-    const base = e.target;
-    if (e.target.options.value === 'Base 2') {
-        convertNumberBaseOf(2);
-    }
-})
 
 convertBtn.addEventListener('click', (e) => {
     digitInputs.forEach(input => {
         if (input.value === '') {
             input.value = 0;
         }
-    })
-    convertNumberBaseOf(2);
+    });
+
+    if (numberLengthInput.value === '') {
+        numberLengthInput.value = 1;
+    }
+    const result = convertNumberBaseOf(2);
+    const resultContainer = document
+        .querySelector('.resultContainer');
+
+    resultContainer.firstElementChild.innerText = String(result);
 })
